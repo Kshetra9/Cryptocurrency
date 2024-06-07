475b044f16b5abe4add310ba560dcf1480afde8d
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2024-06-07 19:31:02 +0530

    adding db files

---
+++ create_db.py
@@ -0,0 +1,21 @@
+# create_db.py
+
+import sqlite3
+
+DB_PATH = 'blockchain.db'
+
+def create_database():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS blockchain (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        block_height INTEGER NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    create_database()
