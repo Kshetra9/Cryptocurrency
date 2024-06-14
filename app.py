85b79a1255854dfbec41d631cd464e2a1ed42cd2
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2024-06-14 17:57:22 +0530

    code to render additional metrics

--- app.py
+++ app.py
@@ -6,42 +6,26 @@ import sqlite3
 app = Flask(__name__)
 DB_PATH = 'blockchain.db'
 
-def get_latest_block_height():
+@app.route('/api/blockchain_metrics', methods=['GET'])
+def get_blockchain_metrics():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute('SELECT block_height, timestamp FROM blockchain ORDER BY id DESC LIMIT 1')
-    result = cursor.fetchone()
+    cursor.execute('SELECT block_height, network_hash_rate, difficulty, mempool_size FROM blockchain ORDER BY id DESC LIMIT 1')
+    row = cursor.fetchone()
     conn.close()
-    return result if result else (None, None)
 
-# Function to update the block height in the SQLite database
-# def update_db_block_height(height):
-#     conn = sqlite3.connect('block_height.db')
-#     cursor = conn.cursor()
-#     cursor.execute('INSERT INTO block_height (height) VALUES (?)', (height,))
-#     conn.commit()
-#     conn.close()
+    keys = ['block_height', 'network_hash_rate', 'difficulty', 'mempool_size']
+    values = row or [None] * 4
 
+    return dict(zip(keys, values))
+
+@app.route('/metrics')
+def metrics():
+    return jsonify(get_blockchain_metrics())
 
 @app.route('/')
-# def index():
-#     block_height, timestamp = get_latest_block_height()
-#     return jsonify(block_height=block_height, timestamp=timestamp)
-
 def index():
-    # Get the latest block height from Bitcoin Core
-    block_height,timestamp  = get_latest_block_height()
-    # if block_height is not None:
-    #     # Update the block height in the SQLite database
-    #     update_db_block_height(block_height)
-    # else:
-        # If unable to get the latest block height from Bitcoin Core, retrieve it from the database
-    return render_template('index.html',  block_height=block_height, timestamp=timestamp)
-
-@app.route('/block_height')
-def block_height():
-    block_height, timestamp = get_latest_block_height()
-    return jsonify({'blockHeight': block_height, 'timestamp':timestamp})
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
